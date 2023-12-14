f1b10e25d67b41706db57d5dde0f24d056a6176b
Christian Drappi <christiandrappi@gmail.com>
2023-12-14 14:39:08 -0500

    OK

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-# Re-build our app
 npm run build;
 
 BUILD_DIR=${CODE_PATH}/cdrappi.github.io/;
 
-# Copy our built files to a new repo
 cp -r build/* $BUILD_DIR;
 
 # Handle 404's by copying the HTML but insert something that re-routes via JS
-SCRIPT="<script>document.addEventListener('DOMContentLoaded', function() { var location = window.location.href; var redirect = sessionStorage.redirect; delete sessionStorage.redirect; if (redirect && redirect != location) { history.replaceState(null, null, redirect); }});</script>"
+SCRIPT="<script>document.addEventListener('DOMContentLoaded', function() { var location = window.location; var currentUrl = location.href; var baseurl = location.protocol + '//' + location.host; var redirect = sessionStorage.redirect; delete sessionStorage.redirect; if (redirect && redirect !== currentUrl) { history.replaceState(null, null, redirect); } else { history.replaceState(null, null, baseurl);} }); </script>"
 awk -v script="$SCRIPT" '/<\/body>/ {print substr($0, 1, index($0, "</body>") - 1) "\n" script "\n" substr($0, index($0, "</body>")); next} {print}' "$BUILD_DIR/index.html" > "$BUILD_DIR/404.html"
 
 cd $BUILD_DIR;
