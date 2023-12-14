44e192727744dd85b04d24058c77d8fa1cf73c31
Christian Drappi <christiandrappi@gmail.com>
2023-12-14 14:29:33 -0500

    finally

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Re-build our app
-npm run build;
+# npm run build;
 
 BUILD_DIR=${CODE_PATH}/cdrappi.github.io/;
 
@@ -10,7 +10,7 @@ cp -r build/* $BUILD_DIR;
 
 # Handle 404's by copying the HTML but insert something that re-routes via JS
 SCRIPT="<script>document.addEventListener('DOMContentLoaded', function() { var location = window.location.href; var redirect = sessionStorage.redirect; delete sessionStorage.redirect; if (redirect && redirect != location) { history.replaceState(null, null, redirect); }});</script>"
-awk -v script="$SCRIPT" '/<\/body>/ {print script} {print}' "$BUILD_DIR/index.html" > "$BUILD_DIR/404.html"
+awk -v script="$SCRIPT" '/<\/body>/ {print substr($0, 1, index($0, "</body>") - 1) "\n" script "\n" substr($0, index($0, "</body>")); next} {print}' "$BUILD_DIR/index.html" > "$BUILD_DIR/404.html"
 
 cd $BUILD_DIR;
 commit_message=""
@@ -19,4 +19,4 @@ if [ "$1" != "" ]; then
 fi
 
 
-git add -A :/ && git commit -am commit_message && git push origin master
+# git add -A :/ && git commit -am commit_message && git push origin master
