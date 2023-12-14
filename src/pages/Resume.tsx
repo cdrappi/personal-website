0b88c493cb612817bd8fc5976a907f3668a1ba19
Christian Drappi <christiandrappi@gmail.com>
2023-12-14 15:33:58 -0500

    scroll to code section when going to #/resume#code (#35)

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const CollapsibleSection: React.FC<{ title: React.ReactNode, children: React.ReactNode, openInitially: boolean }> = ({ title, children, openInitially }) => {
     const [isOpen, setIsOpen] = useState(openInitially);
@@ -93,14 +93,14 @@ const SkillsSection: React.FC<{ title: string, skills: string[], openInitially?:
 
 const WorkExperience: React.FC = () => {
     return (
-        <div className="mb-8">
+        <div id="work" className="mb-8">
             <h3 className="text-xl font-semibold text-blue-900 mb-2">Work Experience</h3>
             <ExperienceSection
                 role="Software Engineer"
                 company="Alameda Research"
                 dates="May 2021 - Nov 2022"
                 bullets={[
-                    { text: "1st in LoC, commits, deploys & incident responses during tenure; 1/2 to handle on-call duties" },
+                    { text: "1st in LoC, commits, deploys & incident responses during tenure; 1 of 2 to handle on-call duties" },
                     {
                         text: "Larger projects",
                         subBullets: [
@@ -137,9 +137,9 @@ const WorkExperience: React.FC = () => {
             <ExperienceSection
                 role="Software Engineering Consultant"
                 company={null}
-                dates="Apr 2019 - May 2021, Apr 2023 - present"
+                dates="Apr 2019 - May 2021, Apr 2023 - now"
                 bullets={[
-                    { text: <>Leveraged modern vision-language neural networks for image classification & clustering for <LinkText url="https://creatoriq.com" text="CreatorIQ" /></> },
+                    { text: <>Leveraged vision-language neural networks to cluster & classify images for <LinkText url="https://creatoriq.com" text="CreatorIQ" /></> },
                     { text: <>Built <LinkText url="https://apps.apple.com/us/app/cr%C3%A9ateur/id1544016354" text="Createur" />, an iOS app to help influencers connect with their favorite brands</> },
                     { text: <>Developed food bank routing infrastructure for <LinkText url="https://x.company" text="X Development" /> (formerly Google X)</> },
                     { text: <>Ran data science & ML experiments for <LinkText url="https://brilliant.org" text="Brilliant" />, an online education company</> }
@@ -151,8 +151,8 @@ const WorkExperience: React.FC = () => {
                 includeCompanyComma={false}
                 dates="Oct 2017 - Jul 2019"
                 bullets={[
-                    { text: "Manual & semi-automated market-making & taking, returning slightly over 1000% in 20 months" },
-                    { text: "Built volatility surface models & tools for risk management + order flow monitoring" },
+                    { text: "Semi-automated market-making & taking, returning slightly over 1000% in 20 months" },
+                    { text: "Built quoting engine, volatility surface models and tools to manage risk & monitor order flow" },
                     { text: "Ceased operations in anticipation of the platform closing to American residents" }
                 ]}
                 openInitially={false}
@@ -163,7 +163,7 @@ const WorkExperience: React.FC = () => {
                 dates="Aug 2015 - Nov 2018"
                 bullets={[
                     { text: "Mainly backend dev for both internal and client-facing products" },
-                    { text: "Led small team in commits, LOC, deploys & PRs reviewed in final two years" },
+                    { text: "Led small team in commits, LoC, deploys & PRs reviewed in final two years" },
                 ]}
                 openInitially={false}
             />
@@ -182,7 +182,7 @@ const WorkExperience: React.FC = () => {
 
 const CodeSamples: React.FC = () => {
     return (
-        <div className="mb-8">
+        <div id="code" className="mb-8">
             <h3 className="text-xl font-semibold text-blue-900 mb-2">Code Samples</h3>
             <CollapsibleSection title="NFL PBP sims" openInitially={true}>
                 A play-by-play NFL <LinkText text='simulator' url="https://github.com/cdrappi/nfl_sims" /> written in Rust. Uses Python for data modeling
@@ -196,7 +196,7 @@ const CodeSamples: React.FC = () => {
 
 const TechnicalSkills: React.FC = () => {
     return (
-        <div className="mb-8">
+        <div id="tech" className="mb-8">
             <h3 className="text-xl font-semibold text-blue-900 mb-2">Technical Skills</h3>
             <SkillsSection
                 title="Languages"
@@ -240,6 +240,35 @@ const TechnicalSkills: React.FC = () => {
 
 
 const Resume: React.FC = () => {
+
+    useEffect(() => {
+        // This function will be called when the component mounts
+        const navigateToHash = () => {
+            // Extract the anchor part from the hash
+            const hash = window.location.hash;
+            const anchorId = hash.split('#').pop(); // Get the last part after '#'
+
+            if (anchorId === undefined) {
+                return;
+            }
+            // Scroll to the element with the corresponding ID
+            const element = document.getElementById(anchorId);
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth', inline: 'nearest' });
+            }
+        };
+
+        navigateToHash();
+
+        // Optional: If you want to handle hash changes while on the same page
+        window.addEventListener('hashchange', navigateToHash, false);
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('hashchange', navigateToHash, false);
+        };
+    }, []);
+
     return (
         <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <h2 className="text-2xl font-bold text-slate-700 mb-6">Christian Drappi</h2>
